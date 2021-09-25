bbba255efc351fde3195afee83678c45a48f5525
deepak101010 <74196393+deepak101010@users.noreply.github.com>
2021-09-25 12:33:31 +0530

    Add files via upload

---
+++ Practical3D2.py
@@ -0,0 +1,12 @@
+#Name: Deepak Keshri
+#Class: SYIT/308
+
+num = int(input("Enter a number: "))
+fac = 1
+i = 1
+
+while i <= num:
+    fac = fac * i
+    i = i + 1
+
+print("Factorial of ", num, " is ", fac)
